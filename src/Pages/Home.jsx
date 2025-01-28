086f651ad1fd1ce612c8ae1d04493e6ecba66479
rohitturkar <rohittur2002@gmail.com>
2025-01-28 17:41:53 +0530

    color fix

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -104,16 +104,16 @@ const Home = () => {
   return (
     <div className="min-h-screen ">
       <div className=" mx-auto px-4 sm:px-6 lg:px-8 py-12">
-        <div className="flex flex-col md:flex-row justify-between items-start md:items-center mb-8 space-y-4 md:space-y-0 bg-[#111] p-4 text-white rounded-xl">
+        <div className="flex flex-col md:flex-row justify-between items-start md:items-center mb-8 space-y-4 md:space-y-0 ">
           <div className="flex items-center space-x-3">
             <div className="bg-orange-400 p-3 rounded-lg">
               <IoLibrary className="h-8 w-8 text-white" />
             </div>
             <div>
-              <h1 className="text-3xl font-bold text-white tracking-tight">
+              <h1 className="text-3xl font-bold text-gray-800 tracking-tight">
                 Book Inventory
               </h1>
-              <p className="text-white mt-1">Manage your book shop</p>
+              <p className="text-gray-900 mt-1">Manage your book shop</p>
             </div>
           </div>
 
